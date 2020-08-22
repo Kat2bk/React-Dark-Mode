0caaddc087be0b5c6e1946cbbfe2a11c419d17c4
unknown <Katherine2bk@gmail.com>
2020-08-22 09:46:57 -0700

    set up hook folder, created useLocalStorage file... passed in key and initialValue as parameters, set up state with callback

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,10 @@
+import {useState} from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = localStorage.getItem(key)
+        return item ? JSON.parse(item) : initialValue
+    })
+
+    return [storedValue];
+}
