56a47d10d057dbe7423a13241f41353860ffc6a8
unknown <Katherine2bk@gmail.com>
2020-08-24 13:21:22 -0600

    added media query in styles.scss for dark-mode, preparing useDarkMode hook to accept useMedia hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,14 +3,23 @@ import {useEffect} from "react";
 
  const useDarkMode = (initialValue) => {
     const [values, setValues] = useLocalStorage('darkMode', initialValue);
-    console.log(values)
-        if (values === true) {
+
+    const prefersDarkMode = usePrefersDarkMode();
+    const enabled = typeof values !== 'undefined' ? values : prefersDarkMode;
+
+    useEffect(() => {
+        if (enabled === true) {
             document.body.classList.add('dark-mode')
         } else {
             document.body.classList.remove('dark-mode')
         }
+    }, [enabled])
 
     return [values, setValues]
+}
+
+function usePrefersDarkMode() {
+    return useMedia(['(prefers-color-scheme: dark-mode)'], [true], false);
 }
 
 export default useDarkMode;
