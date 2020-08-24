eee1d827dbddcc0e152c045527164e6f7e630be2
unknown <Katherine2bk@gmail.com>
2020-08-24 11:43:58 -0600

    modified useLocalStorage hook, discovered what was wrong with useDarkMode hook... replaced useDarkMode in if statement to values... I needed to search through the stored value to see if it was true or not. Not the hook itself.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,18 @@
 import {useLocalStorage} from "./useLocalStorage";
+import {useEffect} from "react";
 
-export const useDarkMode = () => {
-    const [values, setValues] = useLocalStorage('darkMode');
+ const useDarkMode = (initialValue) => {
+    const [values, setValues] = useLocalStorage('darkMode', initialValue);
+    console.log(values)
+        if (values === true) {
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
 
     return [values, setValues]
 }
+
+export default useDarkMode;
+
+// did not need useEffect???
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,14 +2,19 @@ import {useState} from "react";
 
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-        const item = localStorage.getItem(key)
-        return item ? JSON.parse(item) : initialValue
+        console.log("useLocalStorage", key, initialValue)
+        // const item = window.localStorage.getItem(key)
+        // return item ? JSON.parse(item) : initialValue
+        if (window.localStorage.getItem(key)) {
+            return JSON.parse(window.localStorage.getItem(key));
+        }
+        window.localStorage.setItem(key, JSON.stringify(initialValue));
     })
 
-    const setValue = (value) => {
-        setStoredValue(value);
-        localStorage.setItem(key, JSON.stringify(value));
-    }
+    // const setValue = (value) => {
+    //     setStoredValue(value);
+    //     window.localStorage.setItem(key, JSON.stringify(value));
+    // }
 
-    return [storedValue, setValue];
+    return [storedValue, setStoredValue];
 }
