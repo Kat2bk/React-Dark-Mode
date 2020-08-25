38b347676b91f5ae67c2d1c8abffee8141c33723
unknown <Katherine2bk@gmail.com>
2020-08-25 16:44:32 -0600

    set up axios call in StatusUpdates.js... trying to figure out problem with map method... added Object.values which seems to fix it, but my props are undefined

--- src/components/StatusUpdates.js
+++ src/components/StatusUpdates.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Update from "./Update";
 
 const StatusUpdates = () => {
+    const [updates, setUpdates] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.coingecko.com/api/v3/status_updates/")
+        .then(response => {
+            setUpdates(response.data)
+        })
+        .catch(error => {
+            console.log("Unable to get data", error)
+        })
+    }, [])
+
     return (
         <div>
+        {Object.values(updates).map(update => (
+            <Update
+            key={Date.now()} 
+            description={update.description}
+            user={update.user}
+            usertitle={update.user_title}
+            project={update.project}
+            />
+        ))}
         </div>
     )
 }
